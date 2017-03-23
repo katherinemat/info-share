6dbf3469c603ef720c2a8e65e85081423e48b0d7
Kaz Matthews and Lawrence Eby <kaz+lawrence@>
2017-03-23 14:03:13 -0700

    Reverse order of blog posts

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
-      articles: this.store.findAll("article"),
+      articles: this.store.query("article", {
+        limitToLast: 5
+      }).then(function(results) {
+        return results.toArray().reverse();
+      }),
       categories: this.store.findAll("category")
     });
   }
